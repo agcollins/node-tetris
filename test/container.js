e1f2750f9a3f759bb0f7e6cbfb7b10b86554aa16
agcollins <agcollins@utexas.edu>
2017-11-15 22:43:24 -0600

    Added more container tests, and fixed a few from before

--- test/container.js
+++ test/container.js
@@ -10,41 +10,72 @@ class CursorBlock extends Block {
 }
 
 describe('Container', () => {
-    describe('cursor in the container', () => {
+    describe('valid cursor movement in the bounds of the container', () => {
         let container = null
 
         beforeEach(() => {
             container = new Container(8, 16).addBlock(new CursorBlock())
         })
 
-        it('should default the cursor to (4, 16)', () => {
-            const expected = [{ x: 4, y: 16 }]
+        it('should default the cursor to (3, 15)', () => {
+            const expected = [{ x: 3, y: 15 }]
 
             assert.deepEqual(container.getCursorPositions(), expected)
         })
 
         it('should allow the cursor to move left', () => {
-            const expected = [{ x: 3, y: 16 }]
+            const expected = [{ x: 2, y: 15 }]
 
-            container.addBlock(new CursorBlock()).moveCursorLeft()
+            container.moveCursorLeft()
 
             assert.deepEqual(container.getCursorPositions(), expected)
         })
 
         it('should allow the cursor to move right', () => {
-            const expected = [{ x: 3, y: 16 }]
+            const expected = [{ x: 4, y: 15 }]
 
-            container.addBlock(new CursorBlock()).moveCursorRight()
+            container.moveCursorRight()
 
             assert.deepEqual(container.getCursorPositions(), expected)
         })
 
         it('should allow the cursor to move down', () => {
-            const expected = [{ x: 4, y: 15 }]
+            const expected = [{ x: 4, y: 14 }]
 
-            container.addBlock(new CursorBlock()).moveCursorDown()
+            container.moveCursorDown()
 
             assert.deepEqual(container.getCursorPositions(), expected)
+        })
+    })
+
+    describe('cursor movement which is blocked by the bounds of the container', () => {
+        let container = null
+        const expected = [{ x: 0, y: 1 }]
+
+        beforEach(() => {
+            container = new Container(1, 1).setCurrentBlock(new CursorBlock())
+        })
+
+        it('should place the cursor at (0, 0)', () => {
+            assert.deepEqual(container.getCursorPositions(), expected)
+        })
+
+        it('should not allow movement left', () => {
+            container.moveCursorLeft()
+
+            assert.deepEqual(container.getCursorPositions(), expected)
+        })
+
+        it('should not allow movement right', () => {
+            container.moveCursorRight()
+
+            assert.deepEqual(container.getCursorPositions(), expected)
+        })
+
+        it('should have no cursor after moving down (collision)', () => {
+            container.moveCursorDown()
+
+            assert.equal(container.getCursorPositions(), null)
         })
     })
 })
